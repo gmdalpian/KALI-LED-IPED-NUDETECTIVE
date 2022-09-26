322b9d0fe4a8d460538e04a651564cca0b319564
gmdalpian <68953800+gmdalpian@users.noreply.github.com>
2022-09-26 14:45:32 -0300

    Sleuthkit compiled

---
+++ kali-config/common/includes.chroot/usr/local/bin/sleuthkit-4.11.1_iped_patch/include/tsk/libtsk.h
@@ -0,0 +1,20 @@
+#ifndef _TSK_LIBTSK_H
+#define _TSK_LIBTSK_H
+/*
+ * The Sleuth Kit
+ *
+ * Brian Carrier [carrier <at> sleuthkit [dot] org]
+ * Copyright (c) 2007-2011 Brian Carrier.  All Rights reserved
+ *
+ * This software is distributed under the Common Public License 1.0
+ */
+
+#include "tsk/base/tsk_base.h"
+#include "tsk/img/tsk_img.h"
+#include "tsk/vs/tsk_vs.h"
+#include "tsk/fs/tsk_fs.h"
+#include "tsk/hashdb/tsk_hashdb.h"
+#include "tsk/auto/tsk_auto.h"
+#include "tsk/pool/tsk_pool.h"
+
+#endif
