e2a0d5172d96ddd21c160ddc9f9ff331eb7d32a8
gmdalpian <gmdalpian@gmail.com>
2025-10-20 09:07:56 -0300

    Update trust_desktop_icons.sh

--- kali-config/common/includes.chroot/etc/profile.d/trust_desktop_icons.sh
+++ kali-config/common/includes.chroot/etc/profile.d/trust_desktop_icons.sh
@@ -1,5 +1,3 @@
 #/bin/bash
-
 # Evita as mensagens do XFCe ao executar icones na area de trabalho
-
 for f in ~/Desktop/*.desktop; do chmod +x "$f" && gio set "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"; done
